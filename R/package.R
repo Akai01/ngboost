b85d666e300d734294341c01b390529c08895238
Resul Akay <resul.akay@quantics.io>
2021-09-13 09:31:58 +0200

    globel env

--- R/package.R
+++ R/package.R
@@ -6,12 +6,15 @@
 #' @name ngboost
 NULL
 
-ngboost <- reticulate::import("ngboost",delay_load = TRUE)
-sklearn <- reticulate::import("sklearn",delay_load = TRUE)
-scores <- reticulate::import("ngboost.scores",delay_load = TRUE)
+ngboost <- NULL
+sklearn <- NULL
+scores <- NULL
 
 #' @export
 .onLoad <- function(libname, pkgname) {
   reticulate::configure_environment(pkgname)
+  ngboost <<- reticulate::import("ngboost",delay_load = TRUE)
+  sklearn <<- reticulate::import("sklearn",delay_load = TRUE)
+  scores <<- reticulate::import("ngboost.scores",delay_load = TRUE)
 }
 
